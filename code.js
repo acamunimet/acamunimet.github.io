79651908d0391bd265286faacd6b9157979319c0
acamunimet <113491255+acamunimet@users.noreply.github.com>
2024-02-12 01:15:51 -0400

    Commit inicial

---
+++ code.js
@@ -0,0 +1,125 @@
+const usuarios = [];
+
+// C L A S E S
+// Clase jugador
+class player {
+
+    name = '';
+    grid = null;
+    victories = 0;
+
+    constructor(nombre){
+        this.name = nombre;
+    }
+
+    addVictory() {
+        this.victories +=1;
+    }
+
+    setGrid(dimension) {
+        this.grid = generateMatrix(dimension);
+    }
+
+    checkNumbers(number) {
+        if (this.grid != null) {
+            
+        }
+    }
+}
+
+// Clase caja de matriz
+class matrix_box {
+
+    checked = false;
+    content = 0;
+
+    constructor(number){
+        this.content = number;
+    };
+
+    getContent() {
+        return  this.content;
+    };
+}
+
+
+// F U N C I O N E S
+
+// Mostrar/Ocultar secciones
+function showHideButton_Display(){
+    var container = document.getElementsByClassName("button_display")[0];
+    if (container.style.visibility == 'hidden') {
+        container.style.visibility = 'visible';
+    } else {
+        container.style.visibility = 'hidden';
+    }
+}
+
+// Comenzar juego
+function startGame(dimension){
+    const ingame_players = [];
+
+    while (ingame_players != 4) {
+        ingame_players.push(handleSubmit())                     // Acumula 4 jugadores para comenzar
+    }
+    let turn = 0;
+    while (turn < 26) {
+        if (turn == 0) {
+            for (let index = 0; index < ingame_players.length; index++) {
+                ingame_players[index].setGrid(dimension);                   // Genera los 4 cartones de juego
+            };
+        }
+        let x = releaseNumber();                                        // Libera el numero de la ronda
+        for (let i = 0; i < ingame_players.length; i++) {
+            ingame_players[i];
+            
+        }
+        turn += 1;
+    };
+
+
+};
+
+
+// Generar usuarios
+function handleSubmit() {
+    const nombre = document.getElementById('input-nombre').value;
+
+    let player = new player(nombre);
+    usuarios.push(player);
+    return player
+}
+
+// Generar numero
+function releaseNumber(){
+    x = Math.floor((Math.random() * 50) + 1);
+    return x;
+}
+
+// Generar matriz
+function generateMatrix(dimension) {            //Dimension es un nro del 3 al 5
+    const matrix = [];
+
+    const matrix_numbers = [];
+    while (matrix_numbers.length != dimension*dimension) {
+        for (let i = 0; i < dimension*dimension; i++) {
+            let x = Math.floor((Math.random() * 50) + 1);       //Seleccion de todos los numeros de la matriz sin repetir
+            while (matrix_numbers.includes(x)) {
+                x = Math.floor((Math.random() * 50) + 1);
+            };
+            matrix_numbers.push(x);
+        };
+    };
+
+    while (matrix.length != dimension) {
+        const matrix_row = [];
+        while (matrix_row.length != dimension) {
+          const y = new matrix_box(matrix_numbers[0]);
+          matrix_row.push(y);                                   //Adicion de las cajas a la matriz de forma NxN
+          matrix_numbers.shift();
+        };
+        matrix.push(matrix_row); 
+      };
+
+    return matrix;
+};
